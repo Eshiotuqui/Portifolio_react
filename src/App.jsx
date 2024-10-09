750374f32794e76c067fe3e334533bf5bb09d781
enzomishiotuqui <eshiotuqui@outlook.com>
2024-10-09 18:59:34 -0300

    responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { useRef } from 'react';
 
 // Componentes
 import Navbar from './components/navbar/navbar';
@@ -16,11 +17,22 @@ import Main from './pages/main/main';
 
 function App() {
   const [theme, setTheme] = useState('dark');
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false); // Estado para controlar a sidebar
 
   const toggleTheme = () => {
     const newTheme = theme === 'dark' ? 'light' : 'dark';
     setTheme(newTheme);
     document.documentElement.setAttribute('data-theme', newTheme);
+  };
+
+  // Função para alternar a sidebar
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+  };
+
+  // Função para fechar a sidebar ao selecionar um item de menu
+  const closeSidebar = () => {
+    setIsSidebarVisible(false);
   };
 
   useEffect(() => {
@@ -30,16 +42,18 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
-        <Header theme={theme} toggleTheme={toggleTheme}/>
-        <AnimatedRoutes theme={theme} toggleTheme={toggleTheme} />
+        {/* Passa as funções para Navbar e Header */}
+        <Navbar isSidebarVisible={isSidebarVisible} closeSidebar={closeSidebar} />
+        <Header theme={theme} toggleTheme={toggleTheme} toggleSidebar={toggleSidebar} />
+        <AnimatedRoutes theme={theme} toggleTheme={toggleTheme} closeSidebar={closeSidebar} />
       </BrowserRouter>
     </div>
   );
 }
 
-function AnimatedRoutes({ theme, toggleTheme }) {
+function AnimatedRoutes({ theme, toggleTheme, closeSidebar }) {
   const location = useLocation();
+  const nodeRef = useRef(null);
 
   return (
     <div className="main">
@@ -49,17 +63,20 @@ function AnimatedRoutes({ theme, toggleTheme }) {
           timeout={300}
           classNames="fade"
           unmountOnExit
+          nodeRef={nodeRef} // Usando a ref no CSSTransition
         >
-          <Routes location={location}>
-            <Route path="/" element={<Main theme={theme} toggleTheme={toggleTheme} />}>
-              <Route index element={<Home theme={theme} toggleTheme={toggleTheme} />} />
-              <Route path="curriculum" element={<Curriculum theme={theme} toggleTheme={toggleTheme} />} />
-              <Route path="curriculum/skills" element={<Curriculum theme={theme} toggleTheme={toggleTheme} />} />
-              <Route path="curriculum/servicos" element={<Curriculum theme={theme} toggleTheme={toggleTheme} />} />
-              <Route path="projetos" element={<Projetos theme={theme} toggleTheme={toggleTheme} />} />
-              <Route path="contato" element={<Contato theme={theme} toggleTheme={toggleTheme} />} />
-            </Route>
-          </Routes>
+          <div ref={nodeRef}> {/* Passando a ref diretamente ao elemento que será animado */}
+            <Routes location={location}>
+              <Route path="/" element={<Main theme={theme} toggleTheme={toggleTheme} />}>
+                <Route index element={<Home theme={theme} toggleTheme={toggleTheme} />} />
+                <Route path="curriculum" element={<Curriculum theme={theme} toggleTheme={toggleTheme} />} />
+                <Route path="curriculum/skills" element={<Curriculum theme={theme} toggleTheme={toggleTheme} />} />
+                <Route path="curriculum/servicos" element={<Curriculum theme={theme} toggleTheme={toggleTheme} />} />
+                <Route path="projetos" element={<Projetos theme={theme} toggleTheme={toggleTheme} />} />
+                <Route path="contato" element={<Contato theme={theme} toggleTheme={toggleTheme} />} />
+              </Route>
+            </Routes>
+          </div>
         </CSSTransition>
       </TransitionGroup>
     </div>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,46 +1,51 @@
 import React from 'react';
 import './header_style.scss';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
-const Header = ({ theme, toggleTheme }) => {
+const Header = ({ theme, toggleTheme, toggleSidebar }) => {
   return (
     <>
-        <div className='home_box'>
-            <NavLink to="/"><h3 className='title_header'>Shiotuqui.</h3></NavLink>
-            <div className="home_social">
-                <span className="home_social-follow">Me siga</span>
-                <div className="home_social_links">
-                    <a href="https://www.linkedin.com/in/enzo-shiotuqui-385324266/" target="_blank" rel="noopener noreferrer" className="home_social_link">
-                    <i className="uil uil-linkedin"></i>
-                    </a>
-                    <a href="https://www.instagram.com/enzo.shiotuqui/" target="_blank" rel="noopener noreferrer" className="home_social_link">
-                    <i className="uil uil-instagram"></i>
-                    </a>
-                    <a href="https://github.com/EnzoMiShiotuqui" target="_blank" rel="noopener noreferrer" className="home_social_link">
-                    <i className="uil uil-github"></i>
-                    </a>
-                </div>
+      <div className="home_box">
+        <NavLink to="/"><h3 className="title_header">Shiotuqui.</h3></NavLink>
+        <div className="home_social">
+          <div className="home_social_links">
+            <a href="https://www.linkedin.com/in/enzo-shiotuqui-385324266/" target="_blank" rel="noopener noreferrer" className="home_social_link">
+              <i className="uil uil-linkedin"></i>
+            </a>
+            <a href="https://www.instagram.com/enzo.shiotuqui/" target="_blank" rel="noopener noreferrer" className="home_social_link">
+              <i className="uil uil-instagram"></i>
+            </a>
+            <a href="https://github.com/EnzoMiShiotuqui" target="_blank" rel="noopener noreferrer" className="home_social_link">
+              <i className="uil uil-github"></i>
+            </a>
+          </div>
 
-                <div className="switch_dark">
-                    <div className="theme-switch-wrapper">
-                    <input
-                        type="checkbox"
-                        id="theme-toggle-checkbox"
-                        className="checkbox"
-                        onChange={toggleTheme}  
-                        checked={theme === 'dark'} 
-                    />
-                    <label className="theme-switch label" htmlFor="theme-toggle-checkbox">
-                        <div className="slider round">
-                        <i className="uil uil-moon icon-moon"></i>
-                        <i className="uil uil-sun icon-sun"></i>
-                        <div className="ball"></div>
-                        </div>
-                    </label>
-                    </div>
+          <div className="switch_dark">
+            <div className="theme-switch-wrapper">
+              <input
+                type="checkbox"
+                id="theme-toggle-checkbox"
+                className="checkbox"
+                onChange={toggleTheme}
+                checked={theme === 'dark'}
+              />
+              <label className="theme-switch label" htmlFor="theme-toggle-checkbox">
+                <div className="slider round">
+                  <i className="uil uil-moon icon-moon"></i>
+                  <i className="uil uil-sun icon-sun"></i>
+                  <div className="ball"></div>
                 </div>
+              </label>
             </div>
+          </div>
+
+          <div className="header_nav">
+            <div className="nav_toggle" id="nav_toggle" onClick={toggleSidebar}>
+              <i className="uil uil-bars"></i>
+            </div>
+          </div>
         </div>
+      </div>
     </>
   );
 }
